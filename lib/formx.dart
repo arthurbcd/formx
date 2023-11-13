9f18bfb7949c289c1757791ba12c2193920c358c
Arthur Miranda <60884977+arthurbcd@users.noreply.github.com>
2023-11-13 12:03:40 -0300

    renamed files to reflect class name

--- lib/formx.dart
+++ lib/formx.dart
@@ -1,7 +1,7 @@
 /// Build, validate and fill forms easily with extended Form and TextFormField.
 library formx;
 
-export '../src/extensions/text_formx_field_addons.dart';
 export '../src/extensions/text_formx_field_copy_with.dart';
 export '../src/widgets/formx.dart';
 export '../src/widgets/text_formx_field.dart';
+export 'src/extensions/text_formx_field_modifiers.dart';
